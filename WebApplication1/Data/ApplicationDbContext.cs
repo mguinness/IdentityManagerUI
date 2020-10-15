c04034e1638b6932f934aab0853d1c6e0103544c
Paulo Goncalves <pgoncalves@kpmg.com>
2020-10-15 16:09:52 +0100

    Fix ApplicationDbContext class, must specify User and Role custom classes. If not, a Add-Migration would creade duplicated foreign keys in the database for the existing related Users and Roles tables,

--- WebApplication1/Data/ApplicationDbContext.cs
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
